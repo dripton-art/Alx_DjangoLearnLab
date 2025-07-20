fc7fb9753d492fce20f2d7c71401bd5b7d49fcfc
ivan <ivankaweesa.biz@gmail.com>
2025-07-20 22:18:52 +0200

    Model Relationships in Django

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -9,12 +9,12 @@ django.setup()
 from relationship_app.models import Author, Book, Library, Librarian
 
 # 1. Query all books by a specific author
-author = Author.objects.get(name="M.K. Rolland")
+author = Author.objects.get(name="Author_name")
 author_books = author.books.all()
 print(f"Books by {author.name}: {[book.title for book in author_books]}")
 
 # 2. List all books in a library
-library = Library.objects.get(name="City Library")
+library = Library.objects.get(name="Library_name")
 library_books = library.book.all()
 print(f"Books in {library.name}: {[book.title for book in library_books]}")
 

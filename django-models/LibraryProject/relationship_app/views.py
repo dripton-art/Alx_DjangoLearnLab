3ac6a43ea6a7ebf9aa2df0ef05a230ce80d1322d
ivan <ivankaweesa.biz@gmail.com>
2025-07-24 15:04:35 +0200

    Role-Based Access Control in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -43,3 +43,15 @@ def is_librarian(user):
 
 def is_admin(user):
     return user.is_superuser
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")

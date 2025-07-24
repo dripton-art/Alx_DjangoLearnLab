ba0db64df0e0053cb568d5695eaa4d0dd99f25ab
ivan kaweesa <lilswings7@gmail.com>
2025-07-24 17:21:51 +0200

    updated

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -14,9 +14,9 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 
-    path('admin/', admin_view.admin_view, name='admin_view'),
-    path('librarian/', librarian_view.librarian_view, name='librarian_view'),
-    path('member/', member_view.member_view, name='member_view'),
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
 
     path('add_book/', views.add_book, name= 'add_book'),
     path('edit_book/', views.change_book, name= 'change_book'),
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -26,28 +26,27 @@ def add_book(request):
             error = "Both title and author are required."
             return render(request, 'relationship_app/add_book.html', {'error': error})
     return render(request, 'relationship_app/add_book.html')
- 
 
 
 @permission_required('relationship_app.can_change_book')
 def change_book(request, book_id):
      book = get_object_or_404(Book, id=book_id)
-    
-    if request.method == 'POST':
+
+     if request.method == 'POST':
         book.title = request.POST.get('title')
         book.author = request.POST.get('author')
         book.save()
         return redirect('list_books')
-    else:
+     else:
         error = "Both title and author are required."
         return render(request, 'relationship_app/add_book.html', {'error': error})
-    return render(request, 'relationship_app/edit_book.html', {'book': book})
+     return render(request, 'relationship_app/edit_book.html', {'book': book})
 
 
 @permission_required('relationship_app.can_delete_book')
 def delete_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
-    
+
     if request.method == 'POST':
         book.delete()
         return redirect('list_books')

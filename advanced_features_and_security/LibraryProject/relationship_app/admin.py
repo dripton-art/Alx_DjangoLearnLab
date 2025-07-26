5149d864a3766da685cb0f5d351674085917eb92
ivan <ivankaweesa.biz@gmail.com>
2025-07-26 19:18:46 +0200

    Custom User Model in Django

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
 from .models import UserProfile
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    # Display fields in the user list view
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'date_of_birth')
+
+    # Add fields to the form when viewing/editing a user
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {
+            'fields': ('date_of_birth', 'profile_photo'),
+        }),
+    )
+
+    # Add fields to the form when adding a new user
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {
+            'fields': ('date_of_birth', 'profile_photo'),
+        }),
+    )
+
 admin.site.register(UserProfile)
+admin.site.register(CustomUser, CustomUserAdmin)

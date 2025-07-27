a4632c422dec73a81439e59aa829897da8bb2f72
ivan kaweesa <lilswings7@gmail.com>
2025-07-27 13:43:05 +0200

    update

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth. import get_user_model
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 # Create your models here.

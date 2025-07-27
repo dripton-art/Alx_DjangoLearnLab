62769df3a3917ace6b36acdeeff2f98c82c175df
ivan <ivankaweesa.biz@gmail.com>
2025-07-27 20:13:21 +0000

    updated

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.list_books, name='list_books'),
-    path('create_book', views.add_book, name='create_book'),
+    path('create_book', views.create_book, name='create_book'),
     path('edit_book', views.edit_book, name='edit_book'),
     path('delete_book', views.delete_book, name='delete_book'),
 ]

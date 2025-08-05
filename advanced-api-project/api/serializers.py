e9230382bb341a9209f33720b65b894bdf940e59
ivan <ivankaweesa.biz@gmail.com>
2025-08-05 22:32:05 +0200

    Custom Serializers in DRF

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -1,13 +1,22 @@
 from rest_framework import serializers
 from .models import Book, Author
+from datetime import date
 
+
+# BookSerializer — serializes all fields
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
+    def validate_publication_year(self, value):
+        current_year = date.today().year
+        if value > current_year:
+            raise serializers.ValidationError('Publication year cannot be in the future.')
+        return value
 
 class AuthorSerializer(serializers.ModelSerializer):
-     author_name = serializers.CharField(source='author.username', read_only=True)
+    books = BookSerializer(many=True, read_only=True)  # Nested BookSerializer to serialize the related books dynamically.
+
      class Meta:
         model = Author
-        fields = 'name'
+        fields = ['name', 'books'] # only show name and nested books

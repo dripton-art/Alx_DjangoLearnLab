706a72cfc0a3d8d52235d889d9dfc1ef305aa7b5
ivan <ivankaweesa.biz@gmail.com>
2025-08-07 21:47:30 +0200

    updated

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-
+from django_filters import rest_framework
 
 # Create your views here.
 class CustomBookCreateView(generics.CreateAPIView):

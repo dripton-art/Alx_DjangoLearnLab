6ccb490184ff6bc88eb829f1aa5ba3b1b152e774
ivan <ivankaweesa.biz@gmail.com>
2025-08-14 15:12:26 +0200

    Blog's User Authentication System

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns[
-    path('login_user', views.login_user, name='login'),
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
 
-def login_user(request):
-    return render(request, 'blog/login.html' {})
+
+def register(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('login')  # After registration, go to login
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
 

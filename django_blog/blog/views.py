905ca0e6f49b8eeaf36e82e1d902d68fe481740a
ivan <ivankaweesa.biz@gmail.com>
2025-08-17 19:15:40 +0200

    Blog Post Management Features

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
-from .forms import UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 from .models import UserProfile
-
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 def register(request):
     form = CustomUserCreationForm()
@@ -33,3 +33,28 @@ def profile(request):
         return redirect("profile")
 
     return render(request, "profile.html", {"profile": profile})
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+    success_url = reverse_lazy('post_list')
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+    success_url = reverse_lazy('post_list')
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post_list')

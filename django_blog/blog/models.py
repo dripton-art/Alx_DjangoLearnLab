6898c10262e17a35962d7997c88689a54055836e
ivan <ivankaweesa.biz@gmail.com>
2025-08-17 19:43:23 +0200

    Comment Functionality to Blog Posts

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -19,3 +19,12 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)  # set once when created
+    updated_at = models.DateTimeField(auto_now=True)      # updates every save
+
+    def __str__(self):
+        return f"Comment by {self.author.username} on {self.post.title}"

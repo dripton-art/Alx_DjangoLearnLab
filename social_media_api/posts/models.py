0da79dcdf55e858200db1292f127c505ee410bdf
ivan kaweesa <lilswings7@gmail.com>
2025-08-24 19:07:42 +0200

    updated

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 # Create your models here.
 
-class Post(model.Model):
-	title = model.CharField(max_length=200)
-	author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+class Post(models.Model):
+	title = models.CharField(max_length=200)
+	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
 	content = models.TextField()
 	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now_=True)
+	updated_at = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
 		return self.title
@@ -16,10 +16,10 @@ class Post(model.Model):
 
 class Comment(models.Model):
 	post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-	author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
 	content = models.TextField()
 	created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_=True)
+	updated_at = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
 		return f'comment from {self.author.username} on {self.post.title}'

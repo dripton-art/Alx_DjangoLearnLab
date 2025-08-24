5924f67ec41a609789954290e435f8177e710a1a
ivan <ivankaweesa.biz@gmail.com>
2025-08-24 19:47:04 +0200

    User Follows and Feed Functionality

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -7,4 +7,4 @@ class CustomUser(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
     followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
-    
+    following = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)

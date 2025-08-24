f58cc29d7f932c9a854b34eda8d3359d58c71474
ivan <ivankaweesa.biz@gmail.com>
2025-08-24 16:54:49 +0200

    User Authentication for a Social Media API

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    bio = models.CharField(max_length=1000, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
     followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
     
